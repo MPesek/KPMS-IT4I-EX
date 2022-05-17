9b4551a4f4597c25f3f6a3a837c57d4f799d04c1
MPesek <99751250+MPesek@users.noreply.github.com>
2022-05-17 23:01:03 +0200

    reset

--- mpi/mnist_rsvd.sh
+++ mpi/mnist_rsvd.sh
@@ -15,6 +15,3 @@ echo "loaded R"
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
 time mpirun --map-by ppr:8:node Rscript mnist_rsvd.R
-time mpirun --map-by ppr:16:node Rscript mnist_rsvd.R
-time mpirun --map-by ppr:32:node Rscript mnist_rsvd.R
-time mpirun --map-by ppr:64:node Rscript mnist_rsvd.R
