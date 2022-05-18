028c4864272492598b352be229bfbf54723888c1
MPesek <99751250+MPesek@users.noreply.github.com>
2022-05-18 17:38:13 +0200

    new times 2

--- mpi/mnist_rsvd.sh
+++ mpi/mnist_rsvd.sh
@@ -14,4 +14,12 @@ echo "loaded R"
 ## Fix for warnings from libfabric/1.12 bug
 module swap libfabric/1.12.1-GCCcore-10.3.0 libfabric/1.13.2-GCCcore-11.2.0 
 
+
+echo -e "\n>>>>>>>> 2 R sessions" >&2
+time mpirun --map-by ppr:2:node Rscript mnist_rsvd.R
+echo -e "\n>>>>>>>> 4 R sessions" >&2
+time mpirun --map-by ppr:4:node Rscript mnist_rsvd.R
+echo -e "\n>>>>>>>> 8 R sessions" >&2
 time mpirun --map-by ppr:8:node Rscript mnist_rsvd.R
+echo -e "\n>>>>>>>> 16 R sessions" >&2
+time mpirun --map-by ppr:16:node Rscript mnist_rsvd.R
